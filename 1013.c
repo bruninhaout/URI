c07260da3c02cfc0300b79486b64222c66f04880
bruninhaout <65495514+bruninhaout@users.noreply.github.com>
2021-05-18 18:34:47 -0300

    Create 1013.c

---
+++ 1013.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int A, B, C;
+  scanf("%d %d %d", &A, &B, &C);
+  int maiorAB = (A + B + abs (A-B))/2;
+  int maior = (maiorAB + C + abs (maiorAB-C))/2;
+  printf("%d eh o maior\n", maior);
+  return 0;
+}
