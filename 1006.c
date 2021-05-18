e66f224612e81167eaaf80e490589df83663406a
bruninhaout <65495514+bruninhaout@users.noreply.github.com>
2021-05-18 18:25:57 -0300

    Create 1006.c

---
+++ 1006.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (){
+	double a, b, c;
+	scanf ("%lf %lf %lf", &a, &b, &c);
+	double media = ((a*2)+(b*3)+(c*5))/10;
+	printf ("MEDIA = %.1lf\n", media);
+	return 0;
+}
