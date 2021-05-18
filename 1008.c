f92900c21705db903a88741181f74ea5525fcbd1
bruninhaout <65495514+bruninhaout@users.noreply.github.com>
2021-05-18 18:31:15 -0300

    Create 1008.c

---
+++ 1008.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+	float a, b, c;
+	scanf ("%f %f %f", &a, &b, &c);
+	float salario = (b*c);
+	float number = a;
+	printf ("NUMBER = %.0f\n", number);
+	printf ("SALARY = U$ %.2f\n", salario);
+return 0;
+}
