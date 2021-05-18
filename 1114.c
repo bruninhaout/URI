6773f03828fc890c114f1570486b25156f5fc1b7
bruninhaout <65495514+bruninhaout@users.noreply.github.com>
2021-05-18 18:47:28 -0300

    Create 1114.c

---
+++ 1114.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int senha;
+  scanf("%d", &senha);
+
+  while (senha != 2002){
+    printf("Senha Invalida\n");
+    scanf("%d", &senha);
+  }
+  printf("Acesso Permitido\n");
+  return 0;
+}
